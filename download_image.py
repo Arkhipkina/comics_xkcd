1f11c6a6808a22a8b4a7d1ac485f186266020818
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2024-04-22 16:43:35 +0300

    Delete download_image.py

--- download_image.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def download_image(url_image, title):
-
-    response = requests.get(url_image)
-    response.raise_for_status()
-
-    with open (f'image\{title}', "wb") as file:
-        file.write(response.content)
